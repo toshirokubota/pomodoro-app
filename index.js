9bc9e949dfb817714114258110f88f549c78476e
Toshiro Kubota <toshiro732@hotmail.com>
2025-04-05 11:28:50 -0400

    still working on the timer

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 
-// const progress_bar = document.getElementById('circular-progress');
-// const progress_ctrl = document.getElementById('progress-ctrl');
-// const interval_seconds = 10;
-// let intervalId;
-// let time_left = interval_seconds;
-
-// console.log(progress_bar, progress_ctrl);
-// progress_ctrl.addEventListener('click', ()=> {
-//     if(progress_ctrl.innerText == 'start' || progress_ctrl.innerText == 'restart') {
-//         progress_ctrl.innerText = 'pause'
-//         intervalId = setInterval(()=> {
-//             time_left --;
-//             console.log(time_left);
-//             let perc = time_left * 100 / interval_seconds;
-//             progress_bar.style.background = `conic-gradient(#7d2ae8 0%, ${perc}%, #ededed ${perc}%)`;
-//             if(time_left <= 0) {
-//                 clearInterval(intervalId);
-//                 time_left = interval_seconds;
-//                 progress_ctrl.innerText = 'start'
-//                 console.log('Done!');
-//             } else {
-//                 //console.log(time_left, `conic-gradient(#7d2ae8 0%, ${perc}%, #ededed ${perc}%)`);
-//             }
-//         }, 1000);
-//     } else {
-//         progress_ctrl.innerText = 'restart';
-//         clearInterval(intervalId);
-//     }
-// });
-
-/* Theme changes */
-const root = document.documentElement;
-root.style.setProperty('--my-variable', 'new-value'); 
-
-const circle = document.querySelector('.progress-ring__circle');
+const progress = document.getElementById('progress-container');
+//const progress_ctrl = document.getElementById('progress-ctrl');
+const circle = progress.querySelector('.progress-ring__circle');
+const circle2 = progress.querySelector('.progress-ring__hazycircle');
 const radius = circle.r.baseVal.value;
-const circumference = 2 * Math.PI * radius;
-const progressText = document.querySelector('.progress-text');
+const progressText = progress.querySelector('.progress-label-perc');
+const timerCommand = progress.querySelector('.progress-label-command');
 
+const circumference = 2 * Math.PI * radius;
 // Initialize
 circle.style.strokeDasharray = `${circumference} ${circumference}`;
 circle.style.strokeDashoffset = circumference;
+
+const interval_seconds = 10;
 
 function setProgress(percent) {
   const offset = circumference - (percent / 100) * circumference;
   circle.style.strokeDashoffset = offset;
   progressText.textContent = `${percent}%`;
+  //console.log('offset: ', offset, circumference, percent);
+
+  let angle = 360 * percent / 100;
+  let aoffset = angle / 2;
+  circle2.style.background = `conic-gradient(from ${aoffset}deg, #2E325A 0deg, #0E112A ${aoffset}deg, #0E112A 180deg, #0E112A ${360-aoffset}deg, #2E325A 360deg)`;
+  //console.log(`conic-gradient(from ${aoffset}deg, #FFF 0deg, #000 90deg, #000 180deg, #000 270deg, #FFF 360deg)`);
 }
+
+let intervalId;
+let time_left = interval_seconds  ;
+//console.log(progress_bar, progress_ctrl);
+progress.addEventListener('click', ()=> {
+
+  if(timerCommand.innerText == 'start' || timerCommand.innerText == 'restart') {
+    timerCommand.innerText = 'pause'
+    intervalId = setInterval(()=> {
+      //progress_bar.style.background = `conic-gradient(#7d2ae8 0%, ${perc}%, #ededed ${perc}%)`;
+      console.log(time_left);
+      let perc = time_left * 100 / interval_seconds;
+      setProgress(perc);
+      if(time_left <= 0) {
+          clearInterval(intervalId);
+          time_left = interval_seconds;
+          timerCommand.innerText = 'start'
+          console.log('Done!');
+      } else {
+          //console.log(time_left, `conic-gradient(#7d2ae8 0%, ${perc}%, #ededed ${perc}%)`);
+          time_left --;
+      }
+    }, 3000);
+    console.log('start: interval id = ', intervalId);
+  } else {
+    timerCommand.innerText = 'restart';
+    clearInterval(intervalId);
+    console.log('pause: interval id = ', intervalId);
+  }
+});
+
+/* Theme changes */
+const root = document.documentElement;
+root.style.setProperty('--my-variable', 'new-value'); 
+
 
 // Example: Set to 65%
 setProgress(65);
